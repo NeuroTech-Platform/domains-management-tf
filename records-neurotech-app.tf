9244664696d836c9c8aed6543439b980b09c0e30
pducoure <64129146+pducoure@users.noreply.github.com>
2024-06-14 11:22:40 +0200

    fix: snow-staging IP with current IP (#6)

--- records-neurotech-app.tf
+++ records-neurotech-app.tf
@@ -45,7 +45,7 @@ resource "exoscale_domain_record" "neurotech_app_snow_staging_a" {
   domain      = exoscale_domain.neurotech_app.id
   name        = "snow-staging"
   record_type = "A"
-  content     = "86.119.29.188"
+  content     = "86.119.29.104"
   ttl         = 300
 }
 
