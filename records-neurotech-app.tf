2a140aaf3239d2eec4f48ddf934bd7993de81eaf
pducoure <64129146+pducoure@users.noreply.github.com>
2024-08-28 16:21:06 +0200

    Update records-neurotech-app.tf
    Update IP snow-staging

--- records-neurotech-app.tf
+++ records-neurotech-app.tf
@@ -45,7 +45,7 @@ resource "exoscale_domain_record" "neurotech_app_snow_staging_a" {
   domain      = exoscale_domain.neurotech_app.id
   name        = "snow-staging"
   record_type = "A"
-  content     = "86.119.28.164"
+  content     = "86.119.28.152"
   ttl         = 300
 }
 
